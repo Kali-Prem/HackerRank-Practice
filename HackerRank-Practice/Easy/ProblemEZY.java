49ead096a855cfff246932942a8c0c906e4b4117
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-01-04 01:05:09 +0530

    Add files via upload

---
+++ HackerRank-Practice/Easy/ProblemEZY.java
@@ -0,0 +1,26 @@
+package Easy;
+
+/*
+Problem: Problem EZY
+Platform: HackerRank
+Difficulty: Easy
+
+Approach:
+- 
+
+Time Complexity: 
+Space Complexity: 
+*/
+
+import java.util.*;
+
+public class ProblemEZY {
+
+    public static void solve() {
+        // Write your solution here
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
